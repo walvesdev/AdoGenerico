3fdc732df7806907eea74bcf966849b3e132a665
Willian <Willian@DESKTOP-GQGDCL9>
2019-08-06 14:19:11 -0300

    Adicionar arquivos de projeto.

---
+++ AdoGenerico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdoGenerico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
